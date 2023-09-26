94666d3f8d2b394bcd7b56a9f12cd2a90027e401
parthpilkhan <parthpilkhann@gmail.com>
2023-09-26 23:16:20 +0530

    added theme

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,31 @@
-import { createTheme } from "@mantine/core";
+import { createTheme, rem } from "@mantine/core";
 
 export const theme = createTheme({
-  /* Put your mantine theme override here */
+  primaryColor: "ocean-blue",
+  colors: {
+    "ocean-blue": [
+      "#7AD1DD",
+      "#5FCCDB",
+      "#44CADC",
+      "#2AC9DE",
+      "#1AC2D9",
+      "#11B7CD",
+      "#09ADC3",
+      "#0E99AC",
+      "#128797",
+      "#147885",
+    ],
+  },
+
+  shadows: {
+    md: "1px 1px 3px rgba(0, 0, 0, .25)",
+    xl: "5px 5px 3px rgba(0, 0, 0, .25)",
+  },
+
+  headings: {
+    fontFamily: "Roboto, sans-serif",
+    sizes: {
+      h1: { fontSize: rem(36) },
+    },
+  },
 });
