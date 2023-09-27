f0a408801836d899ab4c79d43ea3aba7fd312de2
parthpilkhan <parthpilkhann@gmail.com>
2023-09-27 14:45:03 +0530

    setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import "@mantine/core/styles.css";
 import { MantineProvider, Button } from "@mantine/core";
 import { theme } from "./theme";
+import { Provider } from "react-redux";
+
 import Body from "./components/Body";
+import store from "./redux/store";
 
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
-      <Body />
-    </MantineProvider>
+    <Provider store={store}>
+      <MantineProvider theme={theme}>
+        <Body />
+      </MantineProvider>
+    </Provider>
   );
 }
