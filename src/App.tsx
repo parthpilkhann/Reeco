164dd26976ec257cc5e40565aee3521ba8a8e739
parthpilkhan <parthpilkhann@gmail.com>
2023-09-27 11:21:32 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "@mantine/core/styles.css";
 import { MantineProvider, Button } from "@mantine/core";
 import { theme } from "./theme";
+import Navbar from "./components/Navbar";
 
 export default function App() {
-  return <MantineProvider theme={theme}>
-    
-  </MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <Navbar />
+    </MantineProvider>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,39 @@
+import { AppShell, Group, Title, Text, Button } from "@mantine/core";
+import { useHeadroom } from "@mantine/hooks";
+import { IconShoppingCart } from "@tabler/icons-react";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const pinned = useHeadroom({ fixedAt: 120 });
+
+  return (
+    <AppShell
+      header={{ height: 50, collapsed: !pinned, offset: false }}
+      padding="md"
+    >
+      <AppShell.Header style={{ backgroundColor: "green" }}>
+        <Group justify="space-between" px="xl" h="100%">
+          <Group gap="md">
+            <Title order={2} style={{ color: "rgba(255, 255, 255, 1)" }}>
+              Reeco
+            </Title>
+            <Button variant="subtle" color="rgba(255, 255, 255, 1)">
+              Store
+            </Button>
+            <Button variant="subtle" color="rgba(255, 255, 255, 1)">
+              Order
+            </Button>
+            <Button variant="subtle" color="rgba(255, 255, 255, 1)">
+              Analytics
+            </Button>
+          </Group>
+          <Group gap="md">
+            <IconShoppingCart color="white" />
+            <Text style={{ color: "rgba(255, 255, 255, 1)" }}>
+              Hello, James
+            </Text>
+          </Group>
+        </Group>
+      </AppShell.Header>
+    </AppShell>
+  );
 }
