00dad6f36b5d20d3d9abdfc44d902fdd4d5c8eda
parthpilkhan <parthpilkhann@gmail.com>
2023-09-27 13:40:06 +0530

    search

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,10 +1,12 @@
 import { AppShell, rem } from "@mantine/core";
 import DetailsCard from "./DetailsCard";
+import TableWrapper from "./TableWrapper";
 
 export default function MainBody() {
   return (
-    <AppShell.Main pt={`calc(${rem(120)} + var(--mantine-spacing-md))`}>
+    <AppShell.Main  pt={`calc(${rem(120)} + var(--mantine-spacing-md))`}>
       <DetailsCard />
+      <TableWrapper />
     </AppShell.Main>
   );
 }
---
+++ src/components/TableWrapper.tsx
@@ -0,0 +1,55 @@
+import {
+  Paper,
+  TextInput,
+  ActionIcon,
+  Stack,
+  rem,
+  Group,
+  Button,
+  useMantineTheme,
+} from "@mantine/core";
+import { IconSearch, IconArrowRight, IconPrinter } from "@tabler/icons-react";
+
+export default function TableWrapper() {
+  const theme = useMantineTheme();
+
+  return (
+    <Paper shadow="md" p="md">
+      <Stack>
+        <Group justify="space-between">
+          <TextInput
+            radius="xl"
+            size="md"
+            placeholder="Search questions"
+            rightSectionWidth={42}
+            leftSection={
+              <IconSearch
+                style={{ width: rem(18), height: rem(18) }}
+                stroke={1.5}
+              />
+            }
+            rightSection={
+              <ActionIcon
+                size={32}
+                radius="xl"
+                color={theme.primaryColor}
+                variant="filled"
+              >
+                <IconArrowRight
+                  style={{ width: rem(18), height: rem(18) }}
+                  stroke={1.5}
+                />
+              </ActionIcon>
+            }
+          />
+          <Group justify="center">
+            <Button radius="xl" color={theme.colors.green[9]} variant="outline">
+              Add Item
+            </Button>
+            <IconPrinter />
+          </Group>
+        </Group>
+      </Stack>
+    </Paper>
+  );
+}
