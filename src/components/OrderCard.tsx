4841140e43d1626c6e9c67bd5e6ac6ec7cb8eb79
parthpilkhan <parthpilkhann@gmail.com>
2023-09-27 23:46:59 +0530

    added TODO

--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -9,6 +9,14 @@ import {
 
 export default function OrderCard() {
   const theme = useMantineTheme();
+  function handleApproveOrder() {
+    //TODO: add handleApprove logic
+  }
+
+  function handleBackClick() {
+    // TODO: add handleBackClick logic (navigate(-1))
+  }
+
   return (
     <Paper shadow="lg">
       <Group px="xl" justify="space-between">
@@ -17,10 +25,19 @@ export default function OrderCard() {
           <Text fw={700}>Order 35467ACV</Text>
         </Stack>
         <Group>
-          <Button radius="xl" color={theme.colors.green[9]} variant="outline">
+          <Button
+            radius="xl"
+            color={theme.colors.green[9]}
+            variant="outline"
+            onClick={handleBackClick}
+          >
             Back
           </Button>
-          <Button color={theme.colors.green[9]} radius="xl">
+          <Button
+            color={theme.colors.green[9]}
+            radius="xl"
+            onClick={handleApproveOrder}
+          >
             Approve Order
           </Button>
         </Group>
