0a21300775633e1ab62d754fb3d347531f8408b2
parthpilkhan <parthpilkhann@gmail.com>
2023-09-27 10:51:05 +0530

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,7 @@ import { MantineProvider, Button } from "@mantine/core";
 import { theme } from "./theme";
 
 export default function App() {
-  return <MantineProvider theme={theme}>App </MantineProvider>;
+  return <MantineProvider theme={theme}>
+    
+  </MantineProvider>;
 }
